eaa3076650277c9a43b789961b619b0d236e7861
David White <davwhite@davwhite.com>
2022-08-28 17:59:04 -0400

    Cleanup1

--- model_server/model/buildme.sh
+++
@@ -1,2 +0,0 @@
-podman build -t pn-50.davenet.local:8080/repository/simplevis/simplevis-model:$1 .
-podman tag pn-50.davenet.local:8080/repository/simplevis/simplevis-model:$1 pn-50.davenet.local:8080/repository/simplevis/simplevis-model:latest
--- model_server/server/buildme.sh
+++
@@ -1,2 +0,0 @@
-podman build -t pn-50.davenet.local:8080/repository/simplevis/model-serv:$1 .
-podman tag pn-50.davenet.local:8080/repository/simplevis/model-serv:$1 pn-50.davenet.local:8080/repository/simplevis/model-serv:latest
