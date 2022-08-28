565f001e6ec7d403bead65aa64ddc19099bb81b2
David White <davwhite@davwhite.com>
2022-08-28 10:44:17 -0400

    Loads class file for weights file

--- fastapi/model/main.py
+++ fastapi/model/main.py
@@ -2,8 +2,27 @@
 # uvicorn app:app --reload --host localhost --host 0.0.0.0 --port 9000
 
 import subprocess
+import yaml
 from fastapi import FastAPI, Path, UploadFile
 from typing import Optional
+from yaml import load, dump
+import os
+
+UPLOAD_DIR = "uploaded-files"
+DETECT_DIR = "detected-files"
+PRE_TRAINED = "yolov5s.pt"
+CST_TRAINED = "coco_uavs.pt"
+PRE_CLASSES = "coco128.yaml"
+CST_CLASSES = "uavs2.yaml"
+OBJECT_CLASSES = {}
+
+# Load the classes
+with open("data/" + CST_CLASSES, 'r') as stream:
+    try:
+        parsed_yaml=yaml.safe_load(stream)
+        OBJECT_CLASSES = parsed_yaml['names']
+    except yaml.YAMLError as exc:
+        print(exc)    
 
 app = FastAPI()
 
@@ -15,14 +34,14 @@ def index():
 def detect(file: UploadFile):
     try:
         contents = file.file.read()
-        with open("uploaded-files/" + file.filename, 'wb') as f:
+        with open(UPLOAD_DIR + "/" + file.filename, 'wb') as f:
             f.write(contents)
     except Exception as err:
         return {"error": err}
     finally:
         file.file.close()
     
-    result = subprocess.run(['python', 'detect.py','--weights','yolov5s.pt','--save-txt','--project',"detected-files",'--exist-ok','--source',"uploaded-files/" + file.filename], stdout=subprocess.PIPE)
+    result = subprocess.run(['python', 'detect.py','--weights',PRE_TRAINED,'--save-txt','--project',DETECT_DIR,'--exist-ok','--source',UPLOAD_DIR + "/" + file.filename], stdout=subprocess.PIPE)
     output = str(result.stdout)
     labels = get_labels(file.filename)
 
@@ -32,19 +51,24 @@ def detect(file: UploadFile):
 def detect(file: UploadFile):
     try:
         contents = file.file.read()
-        with open("uploaded-files/" + file.filename, 'wb') as f:
+        with open(UPLOAD_DIR + "/" + file.filename, 'wb') as f:
             f.write(contents)
     except Exception as err:
         return {"error": err}
     finally:
         file.file.close()
     
-    # result = subprocess.run(['python', 'detect.py','--weights','yolov5s.pt','--save-txt','--source',"uploaded-files/" + file.filename], stdout=subprocess.PIPE)
-    result = subprocess.run(['python', 'detect.py','--weights','coco_uavs.pt','--save-txt','--project',"detected-files",'--exist-ok','--source',"uploaded-files/" + file.filename], stdout=subprocess.PIPE)
+    result = subprocess.run(['python', 'detect.py','--weights',CST_TRAINED,'--save-txt','--project',DETECT_DIR,'--exist-ok','--source',UPLOAD_DIR + "/" + file.filename], stdout=subprocess.PIPE)
     output = str(result.stdout)
     labels = get_labels(file.filename)
 
     return {"message": f"Successfully uploaded {file.filename}", "labels": labels}
 
 def get_labels(filename):
+    label_file = os.path.splitext(DETECT_DIR + "/exp/labels/" + filename)[0] + ".txt"
+    print(OBJECT_CLASSES[0])
+    with open(label_file) as file:
+        lines = file.readlines()
+        lines = [line.rstrip() for line in lines]
+        print(lines)
     return { "filename": filename, "object": "someobject", "count": 0 }
