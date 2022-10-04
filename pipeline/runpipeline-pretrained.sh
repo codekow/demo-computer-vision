985b489d3ed43f580eaaaa212e24657f80e78246
David White <davwhite@davwhite.com>
2022-10-04 12:21:52 -0400

    Buildargs

--- pipeline/runpipeline-pretrained.sh
+++ pipeline/runpipeline-pretrained.sh
@@ -5,9 +5,6 @@ volumeClaimTemplateFile=https://raw.githubusercontent.com/openshift/pipelines-tu
 -p deployment-name=simplevis-pretrained \
 -p git-url=https://github.com/redhat-na-ssa/simplevis.git \
 -p git-revision=develop \
--p WEIGHTS=yolov5s.pt \
--p TRAINING_NAME=pretrained \
--p TRAINING_VER=1.0 \
--p MODEL_CLASSES=coco128.yaml \
+-p BUILD_EXTRA_ARGS='--build-arg TRAINING_NAME=pretrained --build-arg TRAINING_VER=1.0 --build-arg WEIGHTS=yolov5s.pt --build-arg MODEL_CLASSES=coco128.yaml' \
 -p IMAGE=image-registry.openshift-image-registry.svc:5000/simplevis/simplevis-pretrained \
 --use-param-defaults
