63c9e1e24ffe1a68d532a3c387d875531eed17a2
Cory Latschkowski <koree@redhat.com>
2024-10-15 14:18:41 -0500

    update: app

--- components/model/main.py
+++ components/model/main.py
@@ -2,18 +2,19 @@
 # https://stackoverflow.com/questions/1523427/what-is-the-common-header-format-of-python-files
 
 from fastapi import FastAPI, APIRouter, Request, Depends
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, FileResponse
 
 from markdown import markdown
 import markdown.extensions.fenced_code
+
 
 def render_markdown(file):
     """Render Markdown Syntax to final HTML."""
     with open(file, "r") as input_file:
         text = input_file.read()
         input_file.close()
-        
-        rendered = markdown.markdown(text, extensions=['fenced_code', 'codehilite'])
+
+        rendered = markdown.markdown(text, extensions=["fenced_code", "codehilite"])
         html = "<html>\n" + rendered + "\n</html>"
 
     return html
@@ -21,18 +22,24 @@ def render_markdown(file):
 
 def create_app():
     app = FastAPI()
+    favicon_path = "favicon.ico"
 
     @app.get(
-    "/", 
-    response_class=HTMLResponse,
-    summary="General Info",
+        "/",
+        response_class=HTMLResponse,
+        summary="General Info",
     )
+    @app.get("/favicon.ico", include_in_schema=False)
+    async def favicon():
+        return FileResponse(favicon_path)
+
     def root():
-        return render_markdown('README.md')
+        return render_markdown("README.md")
 
     import healthz
     import api_v1.detect
     import api_v1.file
+
     # import api_v1.example
 
     app.include_router(healthz.app)
@@ -41,6 +48,7 @@ def create_app():
     # app.include_router(api_v1.example.app, prefix="/v1", tags=["latest"])
 
     return app
+
 
 app = create_app()
 
